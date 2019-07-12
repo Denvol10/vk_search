1ed2044320398d25d0bc9cc2302974a6a0af50ce
Denis Scherbatyuk <denvol100@gmail.com>
2019-07-12 23:00:06 +0700

    add photo function

--- vk_search_bots.py
+++ vk_search_bots.py
@@ -38,15 +38,29 @@ def get_session():
         return
     return vk_session
 
-def main():
-    vk_session = get_session()
+def get_friends_ids(vk_session):
     tools = vk_api.VkTools(vk_session)
-    friends = tools.get_all(method='friends.get', max_count=1, values = {'fields' : 'nickname, domain, sex, bdate, city, country,' 
+    tmp_peop = tools.get_all(method='friends.get', max_count=1, values = {'fields' : 'nickname, domain, sex, bdate, city, country,' 
                                                                         'timezone, photo_50, photo_100, photo_200_orig, has_mobile,' 
                                                                         'contacts, education, online, relation, last_seen, status,' 
                                                                         'can_write_private_message, can_see_all_posts, can_post, universities'})
-    for friend in friends['items']:
-        print(friend['first_name'], friend['last_name'])
+    return tmp_peop.get('items')
+
+def get_photos(owner_id, vk_session):
+    tools = vk_api.VkTools(vk_session)
+    info_photo = tools.get_all(method='photos.get', max_count=100, values = {'owner_id' : owner_id, 'album_id' : 'wall'})
+    return info_photo.get('items')
+
+def main():
+    vk_session = get_session()
+    for man in get_friends_ids(vk_session):
+        print(man['id'], man['last_name'])
+        photos_info = get_photos(owner_id=man['id'], vk_session=vk_session)
+        try:
+            for photo in photos_info:
+                print(photo['date'])
+        except IndexError:
+            continue
 
 
 if __name__ == '__main__':
